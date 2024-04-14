fef79389671d4c11ab0933f9fa2c577be42594cb
CyberSleeper <mahartha.gemilang@gmail.com>
2024-04-14 21:17:22 +0700

    Initial commit

---
+++ src/test/java/id/ac/ui/cs/advprog/bookubeauth/BookUBeAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bookubeauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookUBeAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
