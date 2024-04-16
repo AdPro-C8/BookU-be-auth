37d50098b15b6fec8b04dc9fb3976ba92c646967
CyberSleeper <mahartha.gemilang@gmail.com>
2024-04-17 02:10:35 +0700

    add(repositories): UserRepository

---
+++ src/main/java/id/ac/ui/cs/advprog/bookubeauth/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.bookubeauth.repositories;
+
+import id.ac.ui.cs.advprog.bookubeauth.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
