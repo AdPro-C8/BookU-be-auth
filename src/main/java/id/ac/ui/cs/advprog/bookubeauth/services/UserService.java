f2936eb701020b6bf52cf6049641df931a0e293f
CyberSleeper <mahartha.gemilang@gmail.com>
2024-04-17 02:19:10 +0700

    add(controllers): UserController

--- src/main/java/id/ac/ui/cs/advprog/bookubeauth/services/UserService.java
+++ src/main/java/id/ac/ui/cs/advprog/bookubeauth/services/UserService.java
@@ -1,4 +1,4 @@
-package id.ac.ui.cs.advprog.bookubeauth.service;
+package id.ac.ui.cs.advprog.bookubeauth.services;
 
 import id.ac.ui.cs.advprog.bookubeauth.models.User;
 import id.ac.ui.cs.advprog.bookubeauth.repositories.UserRepository;
