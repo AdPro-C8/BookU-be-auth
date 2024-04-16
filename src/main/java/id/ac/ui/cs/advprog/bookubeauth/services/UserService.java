f4384643692d409c4c7c933bc162b20dcbc1c16a
CyberSleeper <mahartha.gemilang@gmail.com>
2024-04-17 02:10:49 +0700

    add(service): UserService

---
+++ src/main/java/id/ac/ui/cs/advprog/bookubeauth/services/UserService.java
@@ -0,0 +1,24 @@
+// package id.ac.ui.cs.advprog.bookubeauth.service;
+
+// import id.ac.ui.cs.advprog.bookubeauth.model.User;
+// import id.ac.ui.cs.advprog.bookubeauth.repositories.UserRepository;
+
+// import org.springframework.stereotype.Service;
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+// @Service
+// public class UserService {
+//     private final UserRepository userRepository;
+
+//     public UserService(UserRepository userRepository) {
+//         this.userRepository = userRepository;
+//     }
+
+//     public List<User> allUsers() {
+//         List<User> users = new ArrayList<>();
+//         userRepository.findAll().forEach(users::add);
+//         return users;
+//     }
+// }
