6b45e5a9937dc9e534f5c20c2c16feb4aa9ad110
CyberSleeper <mahartha.gemilang@gmail.com>
2024-05-26 06:08:36 +0700

    fix(sonarcloud): CI error

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
+
 plugins {
 	java
     jacoco
@@ -11,6 +16,9 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_21
+    toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
 }
 
 configurations {
